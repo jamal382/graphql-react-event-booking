17eddaf1f96abbb34507105249f0b3fab8eef584
jamal382 <jamal.brotecs@gmail.com>
2019-09-29 22:51:42 +0600

    Custom Event apply

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql',graphqlHttp({
     schema:buildSchema(`
+    type Event {
+        _id:ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+    
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
 
     schema{
@@ -24,14 +41,37 @@ app.use('/graphql',graphqlHttp({
     `),
     rootValue:{
         events: () =>{
-            return ['Romantic Cooking','Sailing','All-Night Coding'];
+            return events;
         },
         createEvent:(args) => {
-            const eventName = args.name;
-            return eventName;
+         const event = {
+            _id:Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date().toISOString()
+         }
+         events.push(event);
+         return event;
         }
     },
     graphiql: true
+/*     http://localhost:3000/graphq
+mutation{
+        createEvent(eventInput:{title:"test titel",description:"Does it works is?",
+          price:5.24,date:"2019-09-29T16:32:20.671Z"}){
+          title
+          description
+        }
+      } 
+------------------
+    query{
+    events{
+    _id
+    description
+  }
+}
+      */
 }));
 
 app.get('/',(req,res,next)=>{
