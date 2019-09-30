cd9c3344a8643dd6b10fa1837727a8f4c9f7d96d
jamal.khan <jamal.khan@brotecs.com>
2019-09-30 14:51:02 +0600

    Data store and get from MongoDB

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+/**
+ * jamal.khan@brotecs.com
+ * bTl123085284
+ * https://cloud.mongodb.com/user?signedOut=true
+ */
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
-
+const mongoose = require('mongoose')
+const Event = require('./models/event');
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
-app.use('/graphql',graphqlHttp({
-    schema:buildSchema(`
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
     type Event {
         _id:ID!
         title: String!
@@ -39,43 +43,65 @@ app.use('/graphql',graphqlHttp({
         mutation: RootMutation
     }
     `),
-    rootValue:{
-        events: () =>{
-            return events;
+    rootValue: {
+        events: () => {
+          return Event.find().then(events=>{
+               return events.map(event=>{
+                   return {...event._doc}
+               });
+           }).catch(err=> {
+               throw err;
+           });
         },
-        createEvent:(args) => {
-         const event = {
-            _id:Math.random().toString(),
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: new Date().toISOString()
-         }
-         events.push(event);
-         return event;
+        createEvent: (args) => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+            return event
+            .save().then(resust => {
+                //console.log(resust);
+                //console.log(...resust._doc);
+                var jamal = {...resust._doc};
+                console.log(resust);
+                return resust;
+
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
         }
     },
     graphiql: true
-/*     http://localhost:3000/graphq
-mutation{
-        createEvent(eventInput:{title:"test titel",description:"Does it works is?",
-          price:5.24,date:"2019-09-29T16:32:20.671Z"}){
-          title
-          description
-        }
-      } 
-------------------
-    query{
-    events{
-    _id
-    description
-  }
-}
-      */
+    /*     http://localhost:3000/graphq
+    mutation{
+            createEvent(eventInput:{title:"test titel",description:"Does it works is?",
+              price:5.24,date:"2019-09-29T16:32:20.671Z"}){
+              title
+              description
+            }
+          } 
+    ------------------
+        query{
+        events{
+        _id
+        description
+      }
+    }
+          */
 }));
 
-app.get('/',(req,res,next)=>{
-res.send('Hello World!');
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
 });
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-kupsy.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+    console.log('Connected');
+}).catch(
+    err => {
+        console.log(err);
+    }
+);
