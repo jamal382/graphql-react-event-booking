b3c8d901b7e258942a1ac502a146c1407f2fb5bb
jamal.khan <jamal.khan@brotecs.com>
2019-10-01 18:34:03 +0600

    Add dynamic relation and and better structure

--- app.js
+++ app.js
@@ -6,122 +6,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const Event = require('./models/event');
-const User = require('./models/user');
-const bcrypt = require('bcryptjs');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-    type Event {
-        _id:ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-    
-    type User {
-        _id: ID!
-        email: String!
-        password: String!
-    }
-
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
-    input UserInput {
-        email: String!
-        password: String!
-    }
-
-    type RootQuery {
-        events: [Event!]!
-    }
-
-    type RootMutation {
-        createEvent(eventInput: EventInput): Event
-        createUser(userInput:UserInput): User
-    }
-
-    schema{
-        query: RootQuery
-        mutation: RootMutation
-    }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find().then(events => {
-                return events.map(event => {
-                    return { ...event._doc }
-                });
-            }).catch(err => {
-                throw err;
-            });
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
-            });
-            return event
-                .save().then(resust => {
-                    //console.log(resust);
-                    //console.log(...resust._doc);
-                    var jamal = { ...resust._doc };
-                    console.log(resust);
-                    return resust;
-
-                }).catch(err => {
-                    console.log(err);
-                    throw err;
-                });
-        },
-        createUser: args => {
-            return bcrypt.hash(args.userInput.password, 12)
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: hashedPassword
-                    });
-                    return user.save();
-                })
-                .then(resust => {
-                    return { ...resust._doc, _id: resust.id }
-                })
-                .catch(err => {
-                    throw err;
-                });
-        }
-    },
-    graphiql: true
-    /*     http://localhost:3000/graphq
-    mutation{
-            createEvent(eventInput:{title:"test titel",description:"Does it works is?",
-              price:5.24,date:"2019-09-29T16:32:20.671Z"}){
-              title
-              description
-            }
-          } 
-    ------------------
-        query{
-        events{
-        _id
-        description
-      }
-    }
-          */
-}));
+app.use(
+    '/graphql',
+    graphqlHttp({
+      schema: graphQlSchema,
+      rootValue: graphQlResolvers,
+      graphiql: true
+    })
+  );
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
